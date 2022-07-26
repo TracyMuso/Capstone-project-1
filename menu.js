93522fecce690e3077785e2dadfe2dbd6192d89a
Elfin Git <musongoletracy@gmail.com>
2022-07-26 13:14:34 +0300

    add js for toggle menu

--- menu.js
+++ menu.js
@@ -1,1 +1,17 @@
+const hamburger = document.querySelector('bi-list');
+const closeMenu = document.querySelector('bi-x');
+const menu = document.querySelector('toggle-menu');
 
+hamburger.addEventListener('click', () => {
+    menu.style.display = 'block';
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.hide()
+})
+
+const aLink = document.getElementsByClassName('link');
+
+aLink.addEventListener('click', () => {
+    menu.hide()
+})
