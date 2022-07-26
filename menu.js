b5d6192b22a895bc24c03297271d3aed6baf2c98
Elfin Git <musongoletracy@gmail.com>
2022-07-26 19:27:37 +0300

    fix linter errors

--- menu.js
+++ menu.js
@@ -3,19 +3,19 @@ const closeMenu = document.querySelector('.bi-x');
 const menu = document.querySelector('.add-menu');
 const logo = document.querySelector('.nav-logo');
 
-const closeItem = function() {
-    menu.classList.toggle('no-display');
-}
+const closeItem = function () {
+  menu.classList.toggle('no-display');
+};
 
-const addMenu = function() {
-    menu.classList.toggle('toggle-menu');
-    logo.classList.toggle('no-display');
-}
+const addMenu = function () {
+  menu.classList.toggle('toggle-menu');
+  logo.classList.toggle('no-display');
+};
 
 hamburger.addEventListener('click', () => {
   addMenu();
 });
 
-closeMenu.addEventListener('click', function() {
-    closeItem();
+closeMenu.addEventListener('click', () => {
+  closeItem();
 });
